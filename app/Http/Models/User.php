87ebab495e7e5d96ccbf04af353d2f66c6763d8a
gnchapman <heystudent@gmail.com>
2015-05-25 16:03:59 +0100

    added retrieve method to User model

--- app/Http/Models/User.php
+++ app/Http/Models/User.php
@@ -8,6 +8,8 @@ class User extends Model
 
 	private $_error	= null;
 
+	//************  READ operations  ************//
+
 	// We check if the $where parameter being passed
 	// is an array.  If not, we assume it's a user ID
 	// and pass the information to the _findOne method.
@@ -34,11 +36,71 @@ class User extends Model
 		return this->error;
 	}
 
-	public function create( $user ) {}
+	// The retrieve method will fetch a list of users.
+	// It supports pagination and geospatial queries.
+	// If the $id and $distance parameters are passed,
+	// it attempts to search for nearby users based
+	// on location.
+	// If $id does not match a record, it returns false.
+	// If the user exists, it prepares a geospatial
+	// query using a MongoDB 2dsphere index. 
 
-	public function remove( $id ) {}
+	public function retrieve( $id, $distance, $limit = 9999, $page = 1 )
+	{
+		if ( !empty( $id ) && !empty( $distance ) )
+		{
+			$this->_where( '_id', $id );
+			$this->_select( 'location' );
+			$user = $this->_findOne( $this->_col );
 
-	public function retrieve( $id, $distance, $limit = 9999, $page = 1 ) {}
+			if ( empty( ( array ) $user ) )
+			{
+				$this->_error = "ERROR_INVALID_USER";
+				return false;
+			}
 
-	public function update( $id, $data ) {}
+			$this->_where( '$and', array(
+					array(
+						'_id'		=>	array( '$ne' => new \MongoId( $id ) )
+						),
+					array(
+						'location'	=> array(
+							'$nearSphere'		=> array(
+								'$geometry'		=> array(
+									'type'			=> "Point",
+									'coordinates'	=> $user->location['coordinates']
+									),
+								'$maxDistance'	=> ( float ) $distance
+								)
+							)
+						)
+				) );
+		}
+
+		$this->_limit( $limit, ( $limit * --$page ) );
+
+		return $this->_find( $this->_col );
+	}
+
+
+	//************  CREATE operations  ************//
+
+	public function create( $user )
+	{
+
+	}
+
+	//************  DELETE operations  ************//
+
+	public function remove( $id )
+	{
+
+	}
+
+	//************  UPDATE operations  ************//
+
+	public function update( $id, $data )
+	{
+		
+	}
 }
