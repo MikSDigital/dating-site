16ec9dea06b5487a498dcbc6e45658a35c4fff55
gnchapman <heystudent@gmail.com>
2015-05-27 11:17:12 +0100

    declared create and destroy functions in SessionController

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -1,12 +1,30 @@
 <?php namespace App\Http\Controllers;
 
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
 
 use Illuminate\Http\Request;
 
-class SessionController extends Controller {
+use App\Http\Models\Session as SessionModel;
+use App\Http\Models\User as UserModel;
 
-	//
+class SessionController extends BaseController {
+
+	private $_model = null;
+
+	public function __construct()
+	{
+		$this->_model	= new SessionModel();
+	}
+
+	public function create( Request $request )
+	{
+
+	}
+
+	public function destroy( $token )
+	{
+		
+	}
 
 }
