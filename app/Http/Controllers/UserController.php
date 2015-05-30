cf48abd1ac24981d86ddf09ce564a76fc86d020c
gnchapman <heystudent@gmail.com>
2015-05-30 12:46:10 +0100

    added retrieve method and comments

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,8 @@ class UserController extends Controller {
 		$this->_model	= new UserModel();
 	}
 
+	//************  CREATE operations  ************//
+
 	public function create( Request $request ) {}
 
 	// To retrieve a record from the system, we
@@ -25,6 +27,8 @@ class UserController extends Controller {
 	// valid session, we return a PERMISSION_DENIED
 	// error with a 403 status code.  Otherwise we
 	// return the user record as a JSON object.
+
+	//************  RETRIEVE operations  ************//
 
 	public function get( Request $request, $id )
 	{
@@ -43,11 +47,52 @@ class UserController extends Controller {
 
 		return $this->_response( $result );
 	}
+
+	// We start by fetching the request parameters,
+	// the user's session token and distance
+	// parameters in particular. This method,
+	// however, does not requires an active session.
+	// If a session is valid, we pass the user id to
+	// the retrieve method of the UserModel class.
+
+	// If a distance parameter is passed in, a
+	// geospatial query is executed. If not, a
+	// regular find query is performed. In case
+	// of errors, we retrieve the error from the
+	// model and return it to the user with a 403
+	// status code. Otherwise, we return an array
+	// containing the found users.
+
+	public function retrieve( Request $request )
+	{
+		$token		= $request->get( 'token' );
+		$distance	= $request->get( 'distance' );
+
+		$session 	= $this->_check_session( $token );
+		$result 	= $this->_model->retrieve( ( isset( $session->user_id ) ? $session->user_id : "" ), $distance, $request->get( 'limit' ), $request->get( 'page' ) );
+		
+		if ( !is_array( $result ) && !$result )
+		{
+			$result 		= new \stdClass();
+			$result->error 	= $this->_model->get_error();
+			$result->status = 403;
+		}
+
+		return $this->_response( $result );
+	}
 	
-	public function remove( Request $request, $id ) {}
+	//************  UPDATE operations  ************//
 	
-	public function retrieve( Request $request ) {}
-	
-	public function update( Request $request, $id ) {}
+	public function update( Request $request, $id )
+	{
+
+	}
+
+	//************  DELETE operations  ************//
+
+	public function remove( Request $request, $id )
+	{
+
+	}
 
 }
